5d49c6d8306469178f05604cdb6438d731c8a036
jnovoa29 <jordan.novoa15@gmail.com>
2018-05-24 17:49:50 -0700

    minor change

--- models/scrap.js
+++ models/scrap.js
@@ -3,7 +3,10 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var scrap = new Schema({
-    
+    title: {
+        type: String
+    }
+
 });
 
 module.exports = mongoose.model('scrap', scrap);
